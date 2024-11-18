2ed903e63eef28eac4b7f971fae964d9bf4ba251
Sivan Becker <sbecker@fireblocks.com>
2024-11-18 19:42:20 +0200

    remove old files and add numbers to labels

--- 64cdd390-6bb7-4a8b-b0e0-b52294368613/2024-nov-en-numbers.js
+++ 64cdd390-6bb7-4a8b-b0e0-b52294368613/2024-nov-en-numbers.js
@@ -1,20 +1,20 @@
 words = [
-    { text: "a hundred", translation: 'מאה', partOfSpeech: 'noun' },
-    { text: "eighty", translation: 'שמונים', partOfSpeech: 'adjective' },
-    { text: "eighteen", translation: 'שמונה עשרה', partOfSpeech: 'verb' },
-    { text: "fifteen", translation: 'חמש עשרה', partOfSpeech: 'noun' },
-    { text: "fifty", translation: 'חמישים', partOfSpeech: 'adjective' },
-    { text: "forty", translation: 'ארבעים', partOfSpeech: 'verb' },
-    { text: "fourteen", translation: 'ארבע עשרה', partOfSpeech: 'noun' },
-    { text: "nineteen", translation: 'תשע עשרה', partOfSpeech: 'adjective' },
-    { text: "ninety", translation: 'תשעים', partOfSpeech: 'verb' },
-    { text: "seventeen", translation: 'שבע עשרה', partOfSpeech: 'adjective' },
-    { text: "seventy", translation: 'שבעים', partOfSpeech: 'verb' },
-    { text: "sixteen", translation: 'שש עשרה', partOfSpeech: 'verb' },
-    { text: "sixty", translation: 'שישים', partOfSpeech: 'verb' },
-    { text: "thirteen", translation: 'שלוש עשרה', partOfSpeech: 'verb' },
-    { text: "thirty", translation: 'שלושים', partOfSpeech: 'verb' },
-    { text: "twenty", translation: 'עשרים', partOfSpeech: 'verb' }
+    { text: "a hundred", translation: 'מאה (100)', partOfSpeech: 'noun' },
+    { text: "eighty", translation: 'שמונים (80)', partOfSpeech: 'adjective' },
+    { text: "eighteen", translation: 'שמונה עשרה (18)', partOfSpeech: 'verb' },
+    { text: "fifteen", translation: 'חמש עשרה (15)', partOfSpeech: 'noun' },
+    { text: "fifty", translation: 'חמישים (50)', partOfSpeech: 'adjective' },
+    { text: "forty", translation: 'ארבעים (40)', partOfSpeech: 'verb' },
+    { text: "fourteen", translation: 'ארבע עשרה (14)', partOfSpeech: 'noun' },
+    { text: "nineteen", translation: 'תשע עשרה (19)', partOfSpeech: 'adjective' },
+    { text: "ninety", translation: 'תשעים (90)', partOfSpeech: 'verb' },
+    { text: "seventeen", translation: 'שבע עשרה (17)', partOfSpeech: 'adjective' },
+    { text: "seventy", translation: 'שבעים (70)', partOfSpeech: 'verb' },
+    { text: "sixteen", translation: 'שש עשרה (16)', partOfSpeech: 'verb' },
+    { text: "sixty", translation: 'שישים (60)', partOfSpeech: 'verb' },
+    { text: "thirteen", translation: 'שלוש עשרה (13)', partOfSpeech: 'verb' },
+    { text: "thirty", translation: 'שלושים (30)', partOfSpeech: 'verb' },
+    { text: "twenty", translation: 'עשרים (20)', partOfSpeech: 'verb' }
 
 
 
